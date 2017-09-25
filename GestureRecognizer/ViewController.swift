449c01071c2969c1d4f9412f1d39079496b19a24
Daymein Gregorio <daymeingregorio@Daymeins-MacBook-Pro.local>
2017-09-25 09:17:21 +0900

    First commit

--- GestureRecognizer/ViewController.swift
+++ GestureRecognizer/ViewController.swift
@@ -10,16 +10,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var appIcon: UIView!
+    @IBOutlet weak var trash:   UIView!
+    
+    private var appOrigin: CGPoint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        appOrigin = appIcon.frame.origin
+        addPanGestureRecognizerTo(subView: appIcon)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func addPanGestureRecognizerTo(subView: UIView) {
+        let panGestRec = UIPanGestureRecognizer(target: self, action: #selector(handleDrag(_:)))
+        subView.isUserInteractionEnabled = true
+        subView.addGestureRecognizer(panGestRec)
     }
-
+    
+    @objc internal func handleDrag(_ sender: UIPanGestureRecognizer) {
+        view.bringSubview(toFront: appIcon)
+        let state = sender.state
+        
+        switch state {
+        case .began, .changed:
+            let translation = sender.translation(in: view)
+            appIcon.center = CGPoint(x: appIcon.center.x + translation.x,
+                                     y: appIcon.center.y + translation.y)
+            sender.setTranslation(.zero, in: view)
+        case .ended:
+            if appIcon.frame.intersects(trash.frame) {
+                disposeOfApp()
+            } else {
+                resetIconPosition()
+            }
+        default:
+            break
+        }
+    }
+    
+    private func resetIconPosition() {
+        UIView.animate(withDuration: 0.3) { [unowned self] in
+            self.appIcon.frame.origin = self.appOrigin
+        }
+    }
+    
+    private func disposeOfApp() {
+        UIView.animate(withDuration: 0.18) { [unowned self] in
+            self.appIcon.transform = CGAffineTransform(scaleX: 0.3, y: 0.3)
+            self.appIcon.center = self.trash.center
+            self.appIcon.alpha = 0
+        }
+    }
+    
+    @IBAction func reset() {
+        appIcon.transform = CGAffineTransform.identity
+        appIcon.frame.origin = appOrigin
+        appIcon.alpha = 1
+    }
 
 }
 
